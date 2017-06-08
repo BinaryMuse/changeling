0988a37efb163ce7f6a2a3aa0c9f34440b32075e
Michelle Tilley <binarymuse@github.com>
2017-06-08 14:59:32 -0700

    Extract app component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './containers/app';
+
 ReactDOM.render(
-  <div>Ready</div>,
+  <App />,
   document.getElementById('app'),
 );
